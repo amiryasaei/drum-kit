84e3ed204afea0c29b4717bb50e101a6fb39a79d
Amir Yasaei <amiru@iMac.local>
2026-09-10 14:04:54 -0400

    Completed the drum kit using vanila javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+window.addEventListener("keydown", (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; //stops the null which can cause to error
+  audio.currentTime = 0; //rewind to the start
+  audio.play();
+  key.classList.add("playing");
+});
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) =>
+  key.addEventListener("transitionend", (e) => {
+    if (e.propertyName !== "transform") return;
+    key.classList.remove("playing");
+  }),
+);
